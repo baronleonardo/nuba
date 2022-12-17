f3fdd2e22578e9a64adde31fe9b2bdb76c082638
Baron Leonardo <mohamed.a.elmeligy@protonmail.com>
2022-12-17 05:46:02 +0200

    init the project

---
+++ shell.nix
@@ -0,0 +1,12 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell
+{
+    name = "nuba-env";
+    buildInputs = with pkgs; [
+        rustc
+        cargo
+    ];
+
+    RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+}
